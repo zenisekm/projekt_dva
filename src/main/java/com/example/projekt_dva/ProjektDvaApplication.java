fd7da7d29361910006de3ec563fa63a5edb8026b
zenisekm <zenisek96@gmail.com>
2024-07-03 19:31:55 +0200

    Základy.

---
+++ src/main/java/com/example/projekt_dva/ProjektDvaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekt_dva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektDvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektDvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projekt_dva/ProjektDvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt_dva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektDvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
