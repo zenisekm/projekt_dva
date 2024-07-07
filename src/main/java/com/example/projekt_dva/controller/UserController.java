42867c981b597d4906ef3c47e4a855ea8e011893
zenisekm <zenisek96@gmail.com>
2024-07-07 20:48:46 +0200

    Vráceno do původního stavu, stále nejde testovat.

--- src/main/java/com/example/projekt_dva/controller/UserController.java
+++ src/main/java/com/example/projekt_dva/controller/UserController.java
@@ -2,15 +2,30 @@ package com.example.projekt_dva.controller;
 
 import com.example.projekt_dva.model.User;
 import com.example.projekt_dva.service.UserService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/users/v1")
 public class UserController {
+
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
+
+
+
+    /// test na web
+        @GetMapping("/test")
+        public String testEndpoint() {
+            logger.info("Received request to test endpoint");
+            return "Test endpoint is working!";
+        }
+
+    ///
+
 
     @Autowired
     private UserService userService;
